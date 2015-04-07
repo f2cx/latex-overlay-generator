1b1d998a08ca1dbcd2a2223a1591f3858c8370a0
Fabian Fischer <fabian@virtual-dev.de>
2015-04-07 23:05:56 +0200

    Add textual overlay

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -45,10 +45,10 @@ angular
     $rootScope.overlay.file = "black-demo.png";
 
     $rootScope.overlay.items = [
-      {macro: "annotatedFigureBox", label: "A", position: "bl", x0: 0.01, y0: 0.7, x1: 0.32, y1: 0.89, x2: 0.01, y2: 0.7, },
-      {macro: "annotatedFigureBox", label: "B", position: "bl", x0: 0.32, y0: 0.19, x1: 0.4, y1: 0.3, x2: 0.32, y2: 0.19},
-      {macro: "annotatedFigureBox", label: "C", position: "bl", x0: 0.49, y0: 0.8, x1: 0.53, y1: 0.89, x2: 0.49, y2: 0.8},
-      {macro: "annotatedFigureBox", label: "D", position: "bl", x0: 0.5, y0: 0.18, x1: 0.7, y1: 0.3, x2: 0.5, y2: 0.18}
+      {type: "annotatedFigureBox", label: "A", position: "bl", x0: 0.01, y0: 0.7, x1: 0.32, y1: 0.89, x2: 0.01, y2: 0.7, },
+      {type: "annotatedFigureBox", label: "B", position: "bl", x0: 0.32, y0: 0.19, x1: 0.4, y1: 0.3, x2: 0.32, y2: 0.19},
+      {type: "annotatedFigureBox", label: "C", position: "bl", x0: 0.49, y0: 0.8, x1: 0.53, y1: 0.89, x2: 0.49, y2: 0.8},
+      {type: "annotatedFigureBox", label: "D", position: "bl", x0: 0.5, y0: 0.18, x1: 0.7, y1: 0.3, x2: 0.5, y2: 0.18}
     ];
 
 
